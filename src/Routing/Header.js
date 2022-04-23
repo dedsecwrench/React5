372e9703e7f77a122ae342de6623ffb81e09b9df
Nameera Pathan <66337671+dedsecwrench@users.noreply.github.com>
2022-04-23 17:24:00 +0530

    React5

--- src/Routing/Header.js
+++ src/Routing/Header.js
@@ -4,17 +4,20 @@ import {Link} from 'react-router-dom';
 const Header = () => {
   return (
     <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
+       <nav className="navbar navbar-expand navbar-dark bg-dark">
               <table className="navbar-nav">
                 <tr>
                 <td className="nav-item">
-                  <Link to='/home' className="nav-link" aria-current="page">Home</Link>
+                  <Link to='/home' className="nav-link" aria-current="page">
+                   <button className='buttonz btn-4'><span>Home</span></button></Link>
                 </td>
                 <td className="nav-item">
-                  <Link to='/student' className="nav-link" aria-current="page">Student</Link>
+                  <Link to='/student' className="nav-link" aria-current="page">
+                  <button className='buttonz btn-4'><span>Student</span></button></Link>
                 </td>
                 <td className="nav-item">
-                  <Link to='/contact' className="nav-link" aria-current="page">Contact</Link>
+                  <Link to='/contact' className="nav-link" aria-current="page">
+                  <button className='buttonz btn-4'><span>Contact</span></button></Link>
                 </td>
                 </tr>
               </table>
